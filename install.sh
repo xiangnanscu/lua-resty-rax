b5bfc02e84ea8c1f27e656003a16efe00e68a2e7
xiangnan <280145668@qq.com>
2023-02-13 08:56:37 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+cd /tmp
+rm -rf lua-resty-rax
 git clone https://github.com/xiangnanscu/lua-resty-rax.git --depth=1
 cd lua-resty-rax
 make && INST_PREFIX=/usr/local/openresty make install && make clean
