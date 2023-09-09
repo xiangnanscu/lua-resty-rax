87f5269229183aef4cfeee5fe3bcf514c90b514a
xiangnan <280145668@qq.com>
2023-09-10 06:51:43 +0800

    x

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@ rm -rf lua-resty-rax
 git clone https://github.com/xiangnanscu/lua-resty-rax.git --depth=1
 cd lua-resty-rax
 make && INST_PREFIX=../lualib/xodel make install && make clean
+rm -rf lua-resty-rax
